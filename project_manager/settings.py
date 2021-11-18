f808a814043fafbe72cf1c8f4b30306395552fe0
Petr <Petr@Petr-MBP.home>
2021-11-18 15:23:41 +0100

    Requirements2

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,5 +139,4 @@ if os.getcwd() == '/app':
     DEBUG=False
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
