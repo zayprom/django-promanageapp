d27055178ede82641f110a62220e87fc9c793787
Petr <Petr@Petr-MBP.home>
2021-11-18 19:48:02 +0100

    Minor fix

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,6 +136,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if os.getcwd() == '/app':
     DEBUG=False
-
-# Configure Django App for Heroku.
-django_heroku.settings(locals())
