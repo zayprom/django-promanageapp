47042e1e4c99e5f83f77bdc7b9f503097764542c
Petr <Petr@Petr-MBP.home>
2021-11-19 09:00:12 +0100

    Minor fix

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -135,8 +135,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
